481a34d312a62fef65722d68e8a84afd0f3154a5
sabbbi <s.doornekamp@gmail.com>
2024-02-26 17:37:14 +0100

    betere opzet?

--- src/Dialog/Dialog.stories.jsx
+++ src/Dialog/Dialog.stories.jsx
@@ -1,13 +1,16 @@
 import styled from '@emotion/styled';
 
-import { Dialog } from './Dialog';
-import { StoryLinkWrapper } from '../LinkWrapper';
+// Import the component you want to showcase
+import React from 'react';
+import { Dialog } from './Dialog'; // Adjust the import path as per your project structure
 
-function DialogWrapper(props) {
-  return <CustomDialog {...props} />;
-}
-
+// Story for your component
 export default {
-  title: 'Design System/DialogComponent',
-  component: Dialog,
+  title: 'Components/Dialog', // Title of your component in Storybook
+  component: Dialog, // The component you're showcasing
 };
+
+// Your stories
+export const Default = () => <Dialog />;
+
+export const WithCustomProps = () => <Dialog prop1='value1' prop2='value2' />;
