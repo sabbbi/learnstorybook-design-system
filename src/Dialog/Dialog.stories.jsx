ddb36bfdd47fb588d9a0c8ad0335f9f6f8427fb2
sabbbi <s.doornekamp@gmail.com>
2024-02-26 17:30:30 +0100

    nu wel?

--- src/Dialog/Dialog.stories.jsx
+++ src/Dialog/Dialog.stories.jsx
@@ -3,19 +3,11 @@ import styled from '@emotion/styled';
 import { Dialog } from './Dialog';
 import { StoryLinkWrapper } from '../LinkWrapper';
 
-const CustomDialog = styled.dialog`
-  border: 1px solid green;
-  background: lightgreen;
-  color: rebeccapurple;
-  padding: 1em;
-  font-size: 1.2em;
-`;
-
 function DialogWrapper(props) {
   return <CustomDialog {...props} />;
 }
 
 export default {
-  title: 'Design System/Dialog',
+  title: 'Design System/DialogComponent',
   component: Dialog,
 };
